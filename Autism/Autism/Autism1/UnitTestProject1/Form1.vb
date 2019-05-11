06a343b009a30a4943ffb5dc212145b836f4881a
James <propthevampire@gmail.com>
2019-05-11 13:31:30 +0100

    Unit testing starts

---
+++ Autism/Autism/Autism1/UnitTestProject1/Form1.vb
@@ -0,0 +1,7 @@
+﻿Namespace UnitTestProject1
+    Friend Class Form1
+        Friend Function sum(anger As Integer) As Object
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
